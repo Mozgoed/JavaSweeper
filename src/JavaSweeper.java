267113a772eeff8eb9c70d89e2a76395f16b8654
Алексей Кононов <mozgoed123@mail.ru>
2020-04-04 17:48:20 +0300

    Flag class + MouseLeftClick

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Random;
+
 import sweeper.Box;
 import sweeper.Coord;
 import sweeper.Game;
@@ -11,32 +14,49 @@ public class JavaSweeper extends JFrame {
     private JPanel panel;
     private final int COLS = 9;
     private final int ROWS = 9;
-    private final int BOMBS = 10;
+    private final int BOMBS = 5;
     private final int IMAGE_SIZE = 50;
+
     public static void main(String[] args) {
         new JavaSweeper().setVisible(true);
     }
-    private JavaSweeper(){
+
+    private JavaSweeper() {
         game = new Game(COLS, ROWS, BOMBS);
         game.start();
         setImages();
         initPanel();
         initFrame();
     }
-    private void initPanel(){
-        panel = new JPanel(){
+
+    private void initPanel() {
+        panel = new JPanel() {
             @Override
-            protected void paintComponent(Graphics g){
+            protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                for(Coord coord : Ranges.getAllCoords()){
-                    g.drawImage((Image)game.getBox(coord).image, coord.x*IMAGE_SIZE, coord.y*IMAGE_SIZE,this);
+                for (Coord coord : Ranges.getAllCoords()) {
+                    g.drawImage((Image) game.getBox(coord).image, coord.x * IMAGE_SIZE, coord.y * IMAGE_SIZE, this);
                 }
             }
         };
-        panel.setPreferredSize(new Dimension(Ranges.getSize().x * IMAGE_SIZE,Ranges.getSize().y * IMAGE_SIZE));
+
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int x = e.getX() / IMAGE_SIZE;
+                int y = e.getY() / IMAGE_SIZE;
+                Coord coord = new Coord(x,y);
+                if(e.getButton()== MouseEvent.BUTTON1)
+                    game.pressLeftButton(coord);
+                panel.repaint();
+            }
+        });
+
+        panel.setPreferredSize(new Dimension(Ranges.getSize().x * IMAGE_SIZE, Ranges.getSize().y * IMAGE_SIZE));
         add(panel);
     }
-    private void initFrame(){
+
+    private void initFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Miner - https://www.youtube.com/watch?v=PcZYF0g49gM");
         setLocationRelativeTo(null);
@@ -45,11 +65,13 @@ public class JavaSweeper extends JFrame {
         setIconImage(getImage("icon"));
         pack();
     }
-    private void setImages(){
-        for(Box box : Box.values())
+
+    private void setImages() {
+        for (Box box : Box.values())
             box.image = getImage(box.name().toLowerCase());
     }
-    private Image getImage(String name){
+
+    private Image getImage(String name) {
         String filename = "img/" + name + ".png";
         ImageIcon icon = new ImageIcon(getClass().getResource(filename));
         return icon.getImage();
--- src/sweeper/Game.java
+++ src/sweeper/Game.java
@@ -2,18 +2,28 @@ package sweeper;
 
 public class Game {
     private Bomb bomb;
+    private Flag flag;
 
     public Game(int cols, int rows, int bombs) {
         Ranges.setSize(new Coord(cols, rows));
         bomb = new Bomb(bombs);
+        flag = new Flag();
     }
 
     public void start() {
         bomb.start();
+        flag.start();
     }
 
     public Box getBox(Coord coord) {
-        return bomb.get(coord);
+        if(flag.get(coord) == Box.OPENED)
+            return bomb.get(coord);
+        else
+            return flag.get(coord);
+    }
+
+    public void pressLeftButton(Coord coord) {
+        flag.setOpenedToBox(coord);
     }
 }
 //1:41:14
